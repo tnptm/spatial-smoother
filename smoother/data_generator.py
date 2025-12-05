bf7477d036521e0e1f51e9dc1d77669985601368
Toni Patama <tonipat047@gmail.com>
2025-12-05 17:48:14 +0200

    Refactor data point generation functions for improved readability and consistency

--- smoother/data_generator.py
+++ smoother/data_generator.py
@@ -1,32 +1,50 @@
 from random import random, gauss
 from .smooth_main import GridMapDefinition
 
-def random_point_dist(index: int, grid_settings: GridMapDefinition, expected_rate: float, max_population: int) -> tuple:
+
+def random_point_dist(
+    index: int,
+    grid_settings: GridMapDefinition,
+    expected_rate: float,
+    max_population: int,
+) -> tuple:
     """Generate a random data point with population."""
     return (
-        index + 1, # location index
-        random() * grid_settings.cols * grid_settings.grid_size, # X coordinate
-        random() * grid_settings.rows * grid_settings.grid_size, # Y coordinate
-        #0.05 * max_population + random() * max_population* 0.95 , # min population = 5% of max
-        gauss(mu=expected_rate, sigma=expected_rate * 0.5), # gaussian distribution, expected values follow the poisson (numpy) distribution
+        index + 1,  # location index
+        random() * grid_settings.cols * grid_settings.grid_size,  # X coordinate
+        random() * grid_settings.rows * grid_settings.grid_size,  # Y coordinate
+        # 0.05 * max_population + random() * max_population* 0.95 , # min population = 5% of max
+        gauss(
+            mu=expected_rate, sigma=expected_rate * 0.5
+        ),  # gaussian distribution, expected values follow the poisson (numpy) distribution
     )
 
-def random_point_dist_pop(index: int, grid_settings: GridMapDefinition, expected_rate: float, max_population: int) -> tuple:
+
+def random_point_dist_pop(
+    index: int,
+    grid_settings: GridMapDefinition,
+    expected_rate: float,
+    max_population: int,
+) -> tuple:
     """Generate a random data point with population."""
     return (
-        index + 1, # location index
-        random() * grid_settings.cols * grid_settings.grid_size, # X coordinate
-        random() * grid_settings.rows * grid_settings.grid_size, # Y coordinate
-        0.05 * max_population + random() * max_population* 0.95 , # min population = 5% of max
-        gauss(mu=expected_rate, sigma=expected_rate * 0.5), # gaussian distribution, expected values follow the poisson (numpy) distribution
+        index + 1,  # location index
+        random() * grid_settings.cols * grid_settings.grid_size,  # X coordinate
+        random() * grid_settings.rows * grid_settings.grid_size,  # Y coordinate
+        0.05 * max_population
+        + random() * max_population * 0.95,  # min population = 5% of max
+        gauss(
+            mu=expected_rate, sigma=expected_rate * 0.5
+        ),  # gaussian distribution, expected values follow the poisson (numpy) distribution
     )
+
 
 def create_random_location_data(
-    grid_settings: GridMapDefinition, 
+    grid_settings: GridMapDefinition,
     with_population: bool = False,
-    number_of_locations: int=100, 
-    expected_rate: float=100.0,
-    max_population: int=200_000
+    number_of_locations: int = 100,
+    expected_rate: float = 100.0,
+    max_population: int = 200_000,
 ) -> list[tuple]:  # list of (index: int, x, y, population(optional), rate)
     """Create random locations within a grid (index, x, y, population, rate).
     - nrows: Number of rows in the grid.
@@ -37,23 +55,20 @@ def create_random_location_data(
     returns: List of tuples (index, x, y, population, rate).
     """
     data_generators = {
-        'with_population': random_point_dist_pop,
-        'without_population': random_point_dist
+        "with_population": random_point_dist_pop,
+        "without_population": random_point_dist,
     }
 
-    data_generator = data_generators['with_population'] if with_population else data_generators['without_population']
+    data_generator = (
+        data_generators["with_population"]
+        if with_population
+        else data_generators["without_population"]
+    )
 
     rate_data = []
     for loc_id in range(number_of_locations):
-        
-        loc_data = data_generator(
-            loc_id, 
-            grid_settings, 
-            expected_rate, 
-            max_population
-        )
-        
+        loc_data = data_generator(loc_id, grid_settings, expected_rate, max_population)
+
         rate_data.append(loc_data)
 
     return rate_data
-
